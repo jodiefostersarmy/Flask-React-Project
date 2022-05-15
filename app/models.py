c167b72fba7ab209f33b9b80dcc3c24a6cf289f7
Germs <anthony.kwy@gmail.com>
2022-05-15 14:16:52 +1000

    Catching up on each chapter additions

--- app/models.py
+++ app/models.py
@@ -4,6 +4,14 @@ from hashlib import md5
 from flask_login import UserMixin
 from app import db, login
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+# The above followers variable is an auxillary table because it has no data other than foreign keys, 
+# so it is created without an associated model class
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -17,6 +25,14 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', 
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), 
+        lazy='dynamic'
+    )
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -33,6 +49,25 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def follow(self.user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self.user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self,user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+            ).count() > 0
+    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,3 +77,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
