f93a22917d918fb971dc93ba4573a39c79c31d7d
Germs <anthony.kwy@gmail.com>
2022-06-22 23:18:55 +1000

    pagination is complete

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ants.imessage@gmail.com']
+    POSTS_PER_PAGE = 10
